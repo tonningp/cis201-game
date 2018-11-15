f01ddccbfb9fc269f8e389ddb01642b31c819ea7
Paul Tonning <paul.tonning@student.vvc.edu>
2018-11-15 08:49:43 -0800

    update

--- main.cpp
+++ main.cpp
@@ -19,39 +19,27 @@ void shuffle(std::vector<Card> &deck)
     }
 }
 
-
-void fill(std::vector<int> &v,int amount)
-{
-    for(int i=0;i<amount;i++)
-        v.push_back(i);
-}
-
 void fill(std::vector<Card> &deck)
 {
     char suit[] = {'C','D','H','S'};
-    char rank[] = {'A','2','3','4','5','6','7','8','9','0','J','Q','K'};
+    char rank[] = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
 
     for(int i = 0;i < 4;i++)
         for(int j=0;j < 13;j++)
             deck.push_back(Card(rank[j],suit[i]));
 }
 
-void print(std::vector<int> &v)
-{
-    for(int i=0;i<v.size();i++)
-        std::cout << v[i] << ' ';
-    std::cout << std::endl;
-}
 
 void print(std::vector<Card> &c)
 {
     for(int i=0;i<c.size();i++)
     {
-        std::cout << c[i].to_string() << ' ';
-        if(i && i%10 == 0)
+        if(i && i%13 == 0)
             std::cout << std::endl;
+        std::cout << c[i].to_string() << ' ';
    }
-    std::cout << std::endl;
+   std::cout << std::endl;
+   std::cout << std::endl;
 }
 
 int main()
